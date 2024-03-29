4fdc0092aa19d574ad37a1e890e9e2dcf3c76562
Manuel Mauro <manuel.mauro@protonmail.com>
2024-03-29 20:41:50 +0100

    Add window icon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, winit::WinitWindows};
 use nback::{game::GamePlugin, menu::MenuPlugin, splash::SplashPlugin, state::GameState};
+use winit::window::Icon;
 
 fn main() {
     App::new()
@@ -9,12 +10,35 @@ fn main() {
         .add_plugins(MenuPlugin)
         .add_plugins(GamePlugin)
         .add_systems(Startup, setup)
+        .add_systems(Startup, set_window_icon)
         .add_systems(Update, log_transitions)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn set_window_icon(
+    // we have to use `NonSend` here
+    windows: NonSend<WinitWindows>,
+) {
+    // here we use the `image` crate to load our icon data from a png file
+    // this is not a very bevy-native solution, but it will do
+    let (icon_rgba, icon_width, icon_height) = {
+        let image = image::open("assets/icon.png")
+            .expect("Failed to open icon path")
+            .into_rgba8();
+        let (width, height) = image.dimensions();
+        let rgba = image.into_raw();
+        (rgba, width, height)
+    };
+    let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
+
+    // do it for all windows
+    for window in windows.windows.values() {
+        window.set_window_icon(Some(icon.clone()));
+    }
 }
 
 fn log_transitions(mut transitions: EventReader<StateTransitionEvent<GameState>>) {
