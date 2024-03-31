0df8ed1c1e21d3cc85fd2e9313b8d48464b44274
Manuel Mauro <manuel.mauro@protonmail.com>
2024-03-31 10:02:32 +0200

    Add score module

---
+++ src/game/core/score.rs
@@ -0,0 +1,42 @@
+#[derive(Default)]
+pub struct Score {
+    false_pos: usize,
+    true_pos: usize,
+    false_neg: usize,
+    true_neg: usize,
+}
+
+impl Score {
+    pub fn record_fp(&mut self) {
+        self.false_pos += 1;
+    }
+
+    pub fn record_tp(&mut self) {
+        self.true_pos += 1;
+    }
+
+    pub fn record_fn(&mut self) {
+        self.false_neg += 1;
+    }
+
+    pub fn record_tn(&mut self) {
+        self.true_neg += 1;
+    }
+
+    pub fn correct(&self) -> usize {
+        self.true_pos + self.true_neg
+    }
+
+    pub fn wrong(&self) -> usize {
+        self.false_pos + self.false_neg
+    }
+
+    pub fn f1_score(&self) -> f32 {
+        if self.true_pos + self.false_neg == 0 {
+            1.0
+        } else {
+            self.true_pos as f32
+                / (self.true_pos as f32 + 0.5 * (self.false_pos as f32 + self.false_neg as f32))
+        }
+    }
+}
