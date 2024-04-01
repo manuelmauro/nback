ef1a1d8fc1ffec2e5b3092c159422bbc4f20adff
Manuel Mauro <manuel.mauro@protonmail.com>
2024-04-01 11:58:54 +0200

    Remove windo icon

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use bevy::{prelude::*, winit::WinitWindows};
+use bevy::prelude::*;
 use bevy_egui::EguiPlugin;
 use nback::{game::GamePlugin, menu::MenuPlugin, splash::SplashPlugin, state::AppState};
-use winit::window::Icon;
 
 fn main() {
     App::new()
@@ -12,35 +11,12 @@ fn main() {
         .add_plugins(MenuPlugin)
         .add_plugins(GamePlugin)
         .add_systems(Startup, setup)
-        .add_systems(Startup, set_window_icon)
         .add_systems(Update, log_transitions)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-}
-
-fn set_window_icon(
-    // we have to use `NonSend` here
-    windows: NonSend<WinitWindows>,
-) {
-    // here we use the `image` crate to load our icon data from a png file
-    // this is not a very bevy-native solution, but it will do
-    let (icon_rgba, icon_width, icon_height) = {
-        let image = image::open("assets/icon.png")
-            .expect("Failed to open icon path")
-            .into_rgba8();
-        let (width, height) = image.dimensions();
-        let rgba = image.into_raw();
-        (rgba, width, height)
-    };
-    let icon = Icon::from_rgba(icon_rgba, icon_width, icon_height).unwrap();
-
-    // do it for all windows
-    for window in windows.windows.values() {
-        window.set_window_icon(Some(icon.clone()));
-    }
 }
 
 fn log_transitions(mut transitions: EventReader<StateTransitionEvent<AppState>>) {
